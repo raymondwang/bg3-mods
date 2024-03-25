ccbcd10c98df2f58d9cece2bdec3610b4acd70c6
Raymond Wang <r@ymond.wang>
2024-03-24 23:40:55 -0400

    Dynamic Wildheart approaching 1.x

--- Dynamic Wildheart/Mods/Dynamic Wildheart/ScriptExtender/Lua/BootstrapServer.lua
+++ Dynamic Wildheart/Mods/Dynamic Wildheart/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,17 +1,15 @@
---- Prevent rage from ending for a turn when a character swaps between totems.
+--- Prevent rage from ending for a turn after a character swaps between totems.
 --- Rage totems overwrite one another, and the old totem is removed after the
---- new one is applied. This listener reapplies the removed status that prevents
---- rage from ending the turn you applied it.
+--- new one is applied, which also removes `RAGE_STOP_REMOVE`.
+--- Instead of expanding the possibility of conflicts by modifying each status's
+--- `OnRemoveFunctors`, simply restore the rage end preventer status manually.
 --- @param character GUIDSTRING
 --- @param status string
 --- @return nil
 function ExtendRageWhenSwappingTotems(character, status, _, _)
     if Osi.IsStatusFromGroup(status, 'SG_Rage') == 1 then
-        print('STATUS REMOVED: ' .. status)
         if Osi.HasActiveStatusWithGroup(character, 'SG_Rage') == 1 and Osi.HasActiveStatus(character, 'RAGE_STOP_REMOVE') == 0 then
-            print('STATUS IS IN STATUS GROUP')
             Osi.ApplyStatus(character, 'RAGE_STOP_REMOVE', 6, 1, character)
-            print('APPLIED STATUS')
         end
     end
 end
